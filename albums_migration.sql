00e356871db28d6f1e68fd38b6a3f41347eb922a
Michael Meyer <michael.c.meyer26@gmail.com>
2020-10-26 14:27:35 -0500

    fixed some syntax and ensured artist and name cannot be NULL

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-artist VARCHAR(50),
-name VARCHAR(50),
+id INT UNSIGNED NOT NULL,
+artist VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
 release_date INT,
 sales FLOAT,
-genre VARCHAR(25),
+genre VARCHAR(25)
 );
