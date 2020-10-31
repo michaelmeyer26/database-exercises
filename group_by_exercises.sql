9894c0dacf41480606cf7bf2b2e29e69a764322c
Michael Meyer <michael.c.meyer26@gmail.com>
2020-10-31 17:52:08 -0500

    finished group_by exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name;
+
+SELECT first_name, last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name, first_name;
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya'
+GROUP BY gender;
